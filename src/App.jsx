1801584ec6a45d4d5a949dffa4049f1ca0633a7b
Madhav Dodiya <105213289+MadhavDodiya@users.noreply.github.com>
2025-12-29 12:18:55 +0530

    at office

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Home from "./Pages/Home";
+import Shop from "./Pages/Shop";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
     <Header />
     <Home/>
+    <Shop/>
     <Footer/>
     </>
   )
